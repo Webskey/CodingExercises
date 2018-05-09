b357f137c8b47a51a2a0c1dc39cb1f7a4bddf3c1
Webskey <jolo_92@wp.pl>
2018-05-09 12:57:54 +0200

    Added program to find maximum and minimum value in an array of numbers.

---
+++ src/main/java/org/webskey/codingexecises/MaxMinValueInArray.java
@@ -0,0 +1,24 @@
+package org.webskey.codingexecises;
+
+public class MaxMinValueInArray {
+	public static int max(int[] tab) {
+		int max = tab[0];
+		for(int i = 1; i < tab.length; i++)
+			if(tab[i] > max)
+				max = tab[i];
+		return max;
+	}
+
+	public static int min(int[] tab) {
+		int min = tab[0];
+		for(int i = 1; i < tab.length; i++)
+			if(tab[i] < min)
+				min = tab[i];
+		return min;
+	}
+	public static void main(String[] args) {
+		int[] tab = { 11, 13, 5, 65, 64, 45, 23};
+		System.out.println(max(tab));
+		System.out.println(min(tab));		
+	}
+}
