143f27b2aecc63dea4db92422bd97daf34b2b56d
Webskey <jolo_92@wp.pl>
2018-05-11 14:41:15 +0200

    Added program that counts occurrence of parentheses "(" and ")" and checks if numbers are matching

---
+++ src/main/java/org/webskey/codingexecises/MatchingParentheses.java
@@ -0,0 +1,22 @@
+package org.webskey.codingexecises;
+
+public class MatchingParentheses {
+	public static boolean isMatching(String string) {
+		int leftP = 0;
+		int rightP = 0;
+
+		for(int i = 0; i < string.length(); i++) {
+			if(string.charAt(i) == '(')
+				leftP++;
+			else if(string.charAt(i) == ')')
+				rightP++;
+		}
+		return leftP == rightP;
+	}
+	public static void main(String[] args) {		
+		System.out.println(isMatching("()()()(()"));
+		System.out.println(isMatching("((()))"));
+		System.out.println(isMatching("((())))"));
+		System.out.println(isMatching("((()())))((()()())"));
+	}
+}
