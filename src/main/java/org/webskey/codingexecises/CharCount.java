9f4f35cbd1aa2a8586b2179611fcada1d3b3e9c9
Webskey <jolo_92@wp.pl>
2018-05-16 10:23:24 +0200

    Added program that checks if given String has all characters unique

--- src/main/java/org/webskey/codingexecises/CharCount.java
+++ src/main/java/org/webskey/codingexecises/CharCount.java
@@ -6,21 +6,31 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 public class CharCount {
-
-	public static void main(String[] args) {
-		String word = "thneh honnnest  tom";
-		StringBuilder sb = new StringBuilder(word);
+	
+	public static Map<Character, Integer> countChars(String s) {
+		
 		Map<Character, Integer> charMap = new HashMap<>();
-		for(int i = 0; i < sb.length(); i++) {			
-			if(!charMap.containsKey(sb.charAt(i)))
-				charMap.put(sb.charAt(i), 1);
+		
+		for(int i = 0; i < s.length(); i++) {			
+			if(!charMap.containsKey(s.charAt(i)))
+				charMap.put(s.charAt(i), 1);
 			else {
-				int count = charMap.get(sb.charAt(i));
+				int count = charMap.get(s.charAt(i));
 				count ++;
-				charMap.replace(sb.charAt(i), count);
+				charMap.replace(s.charAt(i), count);
 			}
 		}
-		Set<Entry<Character, Integer>> set = charMap.entrySet();
+		
+		return charMap;
+	}
+	public static void main(String[] args) {
+		String word = "thneh honnnest  tom";
+		String word1 = "aabccdeefgghiijkk";
+		
+		Set<Entry<Character, Integer>> set = countChars(word).entrySet();
+		System.out.println(set);
+		
+		set = countChars(word1).entrySet();
 		System.out.println(set);
 	}
 }
---
+++ src/main/java/org/webskey/codingexecises/StringUniqueChars.java
@@ -0,0 +1,23 @@
+package org.webskey.codingexecises;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringUniqueChars {
+	
+	public static boolean hasUniqueChars(String s) {
+		Set<Character> charSet = new HashSet<>();
+
+		for(int i = 0; i < s.length(); i++)
+			charSet.add(s.charAt(i));
+
+		return s.length() == charSet.size();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(hasUniqueChars("abcd"));
+		System.out.println(hasUniqueChars("ananas"));
+		System.out.println(hasUniqueChars("123abc"));
+		System.out.println(hasUniqueChars("1231abc"));
+	}
+}
