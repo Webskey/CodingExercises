623e7cd3a8edc63685497b7dd6c7d4641223be06
Webskey <jolo_92@wp.pl>
2018-05-23 10:09:09 +0200

    Added program that finds and writes all common elements between two arrays

---
+++ src/main/java/org/webskey/codingexecises/CommonElArr.java
@@ -0,0 +1,17 @@
+package org.webskey.codingexecises;
+
+public class CommonElArr {
+
+	public static void common(int[] arr1, int[] arr2) {
+		for(int i = 0; i < arr1.length - 1; i++ ) 
+			for(int j = 0; j < arr2.length - 1; j++) 
+				if( arr1[i] == arr2[j]) 
+					System.out.println(arr1[i]);
+	}
+
+	public static void main(String[] args) {
+		int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8};
+		int[] arr2 = {0, 2, 4, 6, 8, 10};
+		common(arr1, arr2);
+	}
+}
