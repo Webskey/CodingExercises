f375e2f45e9ce92839f8f7a1e0d23500d37bb1f4
Webskey <jolo_92@wp.pl>
2018-06-13 21:42:59 +0200

    added ChristmasTree exercise implementation

---
+++ src/main/java/org/webskey/codingexecises/ChristmasTree.java
@@ -0,0 +1,20 @@
+package org.webskey.codingexecises;
+
+public class ChristmasTree {
+	public static void main(String[] args) {
+		String star = "*";
+		String space = " ";
+		int height = 15;
+		int starNum = 1;
+		while(height > 0) {
+			String treeRow = "";
+			for(int i = height; i > 0; i--)
+				treeRow += space;
+			height--;
+			for(int i = starNum; i > 0; i--)
+				treeRow += star;
+			starNum += 2;
+			System.out.println(treeRow);
+		}
+	}
+}
